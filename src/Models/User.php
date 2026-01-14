915a4b6ebec285896b338a133f5da41eed0a8d04
alikamal234-gif <alikamal0675@gmail.com>
2026-01-14 19:47:16 +0100

    Add Role and User models with properties and methods; initialize AuthService class

--- src/Models/User.php
+++ src/Models/User.php
@@ -1,2 +1,72 @@
 <?php
 
+class User
+{
+    private ?int $id;
+    private string $name;
+    private string $email;
+    private string $password;
+    private Role $role;
+
+    public function __construct(
+        ?int $id,
+        string $name,
+        string $email,
+        string $password,
+        Role $role
+    ) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->email = $email;
+        $this->password = $password;
+        $this->role = $role;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function getEmail()
+    {
+        return $this->email;
+    }
+    public function getPassword()
+    {
+        return $this->password;
+    }
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+
+
+
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+    public function setRole($role)
+    {
+        $this->role = $role;
+    }
+
+
+}
